40a540ffe19cae3d1ce89cc56d7caae9dc4e23ac
bahawal <bahawalsial@gmail.com>
2023-05-31 23:17:16 +0500

    intial commit

---
+++ index_1.js
@@ -0,0 +1,15 @@
+document.getElementById('demo').innerHTML = "asdf"
+// console.log('got here')
+// document.write('dasdsdsa')
+// alert('dsadsa')
+
+
+
+// var a = 10 / 2 * 3 +(5 - 2);
+// a--
+// var b = 20;
+// var c = a + b;
+// console.log(c)
+
+
+
